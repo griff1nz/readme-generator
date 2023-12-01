633d19845764888698d553d538a40efa71ccefaa
Zion Griffin <defendingmymaininbox@gmail.com>
2023-12-01 14:01:12 -0500

    Fixed some bugs and put finishing touches on the program

---
+++ generated-file/README.md
@@ -0,0 +1,25 @@
+# Readme Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of Contents
+[Description](#description)  
+[Usage](#usage)  
+[Credits](#credits)  
+[License](#license)  
+[Tests](#tests)  
+[Questions](#questions)  
+## Description
+This program allows the user to easily generate a README.md file.
+## Installation
+Before running the program, the user must install Inquirer and FS by typing ```npm i inquirer``` or ```npm i fs``` into the terminal.
+## Usage
+To start the program, type ```node index``` into the terminal and answer the prompts. The generated file will be in ./generated-file/.
+## Credits
+Thank you to all the wonderful TA's in the Bootcamp!
+## License
+This Open Source software is released under the [MIT](https://opensource.org/licenses/MIT) license.
+## Tests
+I am testing this right now by screencasting it.
+## Questions
+Please reach out to me if you have any questions.  
+GitHub Username: griff1nz  
+Email: ziproffitt2005@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license = "Apache") {
+  if (license === "Apache") {
     return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
   }
-  else if (license = "MIT") {
+  else if (license === "MIT") {
     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
   }
-  else if (license = "Boost") {
+  else if (license === "Boost") {
     return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
   }
-  else if (license = "Eclipse") {
+  else if (license === "Eclipse") {
     return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
   }
-  else if (license = "Unlicense") {
+  else if (license === "Unlicense") {
     return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
   }
   else {
@@ -24,6 +24,30 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  let stringToReturn = "## License\n";
+  if (license === "Apache") {
+    stringToReturn += "This Open Source software is released under the [Apache](https://opensource.org/licenses/Apache-2.0) license.";
+    return stringToReturn;
+  }
+  else if (license === "MIT") {
+    stringToReturn += "This Open Source software is released under the [MIT](https://opensource.org/licenses/MIT) license.";
+    return stringToReturn;
+  }
+  else if (license === "Boost") {
+    stringToReturn += "This Open Source software is released under the [Boost](https://www.boost.org/LICENSE_1_0.txt) license.";
+    return stringToReturn;
+  }
+  else if (license === "Eclipse") {
+    stringToReturn += "This Open Source software is released under the [Eclipse](https://opensource.org/licenses/EPL-1.0) license.";
+    return stringToReturn;
+  }
+  else if (license === "Unlicense") {
+    stringToReturn += "This Open Source software is released under the [Unlicense](http://unlicense.org/).";
+    return stringToReturn;
+  }
+  else {
+    return "";
+  }
 
 }
 
@@ -31,13 +55,26 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license != 'No License') {
-    
+    return renderLicenseLink(license);
   }
+  else {
+    return "";
+  }
+}
+//renders table of contents
+function renderTableOfContents(data) {
+tableOfContents = "## Table of Contents";
+tableOfContents += "\n[Description](#description)  \n[Usage](#usage)  \n[Credits](#credits)  \n";
+if (data.license != "No License") {
+  tableOfContents += "[License](#license)  \n";
+}
+tableOfContents += "[Tests](#tests)  \n[Questions](#questions)  \n";
+return tableOfContents;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ` + renderLicenseBadge(data.license) + `\n## Description\n${data.description}\n## Usage\n${data.usage}\n## Contributions\n${data.contributions}\n## Tests\n${data.tests}\n## Contact\nUsername: ${data.username}  \nEmail: ${data.email}`;
+  return `# ${data.title}\n` + renderLicenseBadge(data.license) + "\n" + renderTableOfContents(data) + `## Description\n${data.description}\n## Installation\n${data.installation}\n## Usage\n${data.usage}\n## Credits\n${data.contributions}\n` + renderLicenseSection(data.license) + `\n## Tests\n${data.tests}\n## Questions\nPlease reach out to me if you have any questions.  \nGitHub Username: ${data.username}  \nEmail: ${data.email}`;
 }
 
 module.exports = generateMarkdown;
